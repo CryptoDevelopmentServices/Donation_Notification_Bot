c74aed6e9035840a29708e8afc49d16af41fa39f
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2022-11-01 22:37:55 +0000

    overhaul

--- index.js
+++ index.js
@@ -1,46 +1,134 @@
-const express = require("express");
-const Moralis = require("moralis").default;
-const discord = require("discord.js");
-require("dotenv").config();
-const app = express();
-const port = 3000;
-
-const client = new discord.Client({
-    intents: [],
-  });
-
-client.login(process.env.PASS);
-
-app.use(express.json());
-
-app.post("/webhook", async (req, res) => {
-  const { body, headers } = req;
-
-  try {
-    Moralis.Streams.verifySignature({
-      body,
-      signature: headers["x-signature"],
-    });
+const Discord = require('discord.js')
+const ethers = require('ethers')
+// const winston = require('winston')
 
 
-    let from = body.txs[0].fromAddress;
-    let amount = Number(body.txs[0].value / 1E18);
 
-    const channel = await client.channels.fetch(process.env.CHANNEL);
-    channel.send(`New Donation submitted by \`${from}\`, for ${amount.toFixed(4)} BNB!!!!`);
-    channel.send(`Thank you \`${from}\`, for your donation`);
 
-    return res.status(200).json();
-  } catch (e) {
-    console.log("Not Moralis");
-    return res.status(400).json();
+// const logger = winston.createLogger({
+//   level: 'info',
+//   format: winston.format.json(),
+//   defaultMeta: { service: 'user-service' },
+//   transports: [
+//     new winston.transports.File({ filename: 'error.log', level: 'error' }),
+//     new winston.transports.File({ filename: 'combined.log' }),
+//   ],
+// });
+
+if (process.env.NODE_ENV !== 'production') {
+  // logger.add(new winston.transports.Console({
+  //   format: winston.format.simple(),
+  // }));
+}
+
+
+// TODO: Switch on chainId
+//       support version #? have the reference frontend support version #s?
+//       possibly contract addresses to watch
+
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Ready n' Logged in as ${client.user.tag}!`)
+})
+
+
+
+const abi = [
+  {
+    "inputs":[],
+    "stateMutability":"nonpayable",
+    "type":"constructor"
+  },
+  {
+    "anonymous":false,
+    "inputs":
+    [
+      {
+        "indexed":false,
+        "internalType":"address",
+        "name":"from",
+        "type":"address"
+      },
+      {
+        "indexed":false,
+        "internalType":"uint256",
+        "name":"amount",
+        "type":"uint256"
+      }
+    ],
+      "name":"Donate",
+      "type":"event"
+  },
+  {
+    "inputs":[],
+    "name":"newDonation",
+    "outputs":[],
+    "stateMutability":"payable",
+    "type":"function"
   }
-});
+]
 
-Moralis.start({
-  apiKey: process.env.APIKEY,
-}).then(() => {
-  app.listen(port, () => {
-    console.log(`Listening to streams`);
-  });
-});
+
+
+
+let bscProvider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org/', { name: 'binance', chainId: 56 })
+
+const DonationContractAddress = '0xae611bea165249dee17613b067fc25532f422d76'
+const DonationContract = new ethers.Contract(
+  DonationContractAddress,
+  abi,
+  bscProvider
+)
+
+let topic = ethers.utils.id('Donate(address,uint256)')
+// let topic = ethers.utils.id('CompoundPrizePoolCreated(address,address,address)')
+
+let filter = {
+  topics: [topic],
+  fromBlock: 22688852
+}
+
+
+const getLogs = async (_result) => {
+  try {
+    const eventLog = DonationContract.interface.parseLog(
+      _result
+    )
+
+    const newDonation = eventLog.args.newDonation
+    
+
+    return {
+      newDonation
+    }
+  } catch (e) {
+    // logger.error(e)
+  }
+}
+
+bscProvider.on(filter, async (result) => {
+  const newDonationArgs = await getLogs(result)
+
+  console.log(`New Donation Found!`, newDonationArgs.newDonation)
+  // logger.info(`found result!`, newDonationArgs.newDonation)
+
+  sendDiscordMsg(newDonationArgs)
+})
+
+
+bscProvider.resetEventsBlock(7294893)
+
+const sendDiscordMsg = async ({newDonation, req}) => {
+  const { body } = req;
+  let from = body.txs[0].fromAddress;
+  let amount = Number(body.txs[0].value / 1E18);
+  const url = `https://bscscan.com/address/0xae611bea165249dee17613b067fc25532f422d76#tokentxns${newDonation}`
+
+  await client.login(process.env.TOKEN)
+  const channel = await client.channels.fetch('820375466271178765')
+  channel.send(`New Donation submitted by \`${from}\`, for ${amount.toFixed(4)} BNB!! ${url}`)
+  channel.send(`Thank you \`${from}\`, for your donation`);
+
+  console.log(`sent msg with url ${url}`)
+}
