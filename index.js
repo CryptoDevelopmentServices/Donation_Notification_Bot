d933985897b1b4db3db7ce54b2ba48bb6727a476
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2022-12-10 14:43:02 +0000

    update 1

--- index.js
+++ index.js
@@ -1,48 +1,17 @@
-const { Client, Intents } = require('discord.js')
-const ethers = require('ethers')
-const express = require("express");
-const app = express();
-const port = 3000;
+const { Client, Intents } = require('discord.js');
+const ethers = require('ethers');
 require("dotenv").config();
-const winston = require('winston')
-
-
-
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
-
-app.use(express.json());
-
-// TODO: Switch on chainId
-//       support version #? have the reference frontend support version #s?
-//       possibly contract addresses to watch
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
 
 client.on('ready', () => {
-  console.log(`Ready n' Logged in as ${client.user.tag}!`)
-  client.user.setActivity({name: "Watching for donations.."});
-})
-
-
+  console.log(`Ready n' Logged in as ${client.user.tag}!`);
+  client.user.setActivity({ name: `Watching for donations on ${DonationContractAddress}`, type: 'WATCHING' });
+});
 
 const abi = [
   {
@@ -77,28 +46,23 @@ const abi = [
     "stateMutability":"payable",
     "type":"function"
   }
-]
+];
 
-
-
-
-let bscProvider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org/', { name: 'binance', chainId: 56 })
+const bscProvider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org/', { name: 'binance', chainId: 56 });
 
 const DonationContractAddress = '0xae611bea165249dee17613b067fc25532f422d76'
 const DonationContract = new ethers.Contract(
   DonationContractAddress,
   abi,
   bscProvider
-)
+);
 
-let topic = ethers.utils.id('Donate(address,uint256)')
+const topic = ethers.utils.id('Donate(address,uint256)');
 
-
-let filter = {
+const filter = {
   topics: [topic],
   fromBlock: 22688852
-}
-
+};
 
 const getLogs = async (_result) => {
   try {
@@ -115,53 +79,49 @@ const getLogs = async (_result) => {
       amount
     }
   } catch (e) {
-    logger.error(e)
+    console.error(e)
   }
-}
+};
 
 bscProvider.on(filter, async (result) => {
   const newDonationArgs = await getLogs(result)
 
   console.log(`New Donation Found! From`, newDonationArgs.from, `For`, newDonationArgs.amount , `BNB`)
-  logger.info(`found result! From`, newDonationArgs.from, `For`, newDonationArgs.amount, `BNB`)
-  
-  sendDiscordMsg(newDonationArgs)
-})
 
-bscProvider.resetEventsBlock(22688852)
-
-const sendDiscordMsg = async ({from , amount}) => {
-  app.post("newDonationArgs", async (req, res) => {
-  const { body } = req;
-  let from = body.txs[0].fromAddress;
-  let amount = Number(body.txs[0].value / 1E18);
-  const url = `https://bscscan.com/address/0xae611bea165249dee17613b067fc25532f422d76`
-  const donationUrl = `https://bscscan.com/address/0xae611bea165249dee17613b067fc25532f422d76#writeContract`
-  const donationAddress = `https://bscscan.com/address/0x64ecf1f9bd4edf6267f6f4de42ad0979f6127727#internaltx`
-
-  
-  let channel = client.channels.cache.get('820375466271178765')
-  channel.send(`
-    New Donation submitted by \`${from}\`, for ${amount.toFixed(8)} BNB!! ${url} 
-    \n\n Thank you \`${from}\`, for your donation!
-    `)
-  channel.send(`
-    Would you like to help us expand & improve our services?
-    \n If you answered yes, then you can donate any amount you would like using our donation contract
-    \n ${donationUrl}
-    `)
-    channel.send(`
-    \`Note about donations\`
-    \n \`All donations received will be used to help us expand and improve our services\`
-    \n\`Where we use the funds will be down to you guys!!\`
-    You van view what address the donations are going ${donationAddress}
-    `)
-  return res.status(200).json();
-
-  
-})  
-}
-
-app.listen(port, () => {
-  console.log(`Listening for new donations`);
+  sendDiscordMsg(newDonationArgs.from, newDonationArgs.amount);
 });
+
+
+const sendDiscordMsg = async (_from, _amount) => {
+  const channel = client.channels.cache.get('820375466271178765');
+  if (!channel) {
+    return;
+  }
+
+  bscProvider.resetEventsBlock(22688852);
+  channel.send("newDonationArgs", async (req, res) => {
+    const { body } = req;
+    let from = body.txs[0].fromAddress;
+    let amount = Number(body.txs[0].value / 1E18);
+    const url = `https://bscscan.com/address/0xae611bea165249dee17613b067fc25532f422d76`
+    const donationUrl = `https://bscscan.com/address/0xae611bea165249dee17613b067fc25532f422d76#writeContract`
+    const donationAddress = `https://bscscan.com/address/0x64ecf1f9bd4edf6267f6f4de42ad0979f6127727#internaltx`
+    
+    channel.send(`
+      New Donation submitted by \`${from}\`, for ${amount.toFixed(8)} BNB!! ${url} 
+      \n\n Thank you \`${from}\`, for your donation!
+      `);
+    channel.send(`
+      Would you like to help us expand & improve our services?
+      \n If you answered yes, then you can donate any amount you would like using our donation contract
+      \n ${donationUrl}
+      `);
+      channel.send(`
+      \`Note about donations\`
+      \n \`All donations received will be used to help us expand and improve our services\`
+      \n\`Where we use the funds will be down to you guys!!\`
+      You van view what address the donations are going ${donationAddress}
+      `);
+    return res.status(200).json();
+  });  
+};
